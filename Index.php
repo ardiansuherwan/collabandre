773898f3ce1a7eaedb2a5a2c3d9c89cdaae4d38f
ardiansuherwan <55267701+ardiansuherwan@users.noreply.github.com>
2019-10-20 01:09:56 +0700

    Add files via upload

--- Index.php
+++ Index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim</h3>";
-echo "<h3>Ng. Andre Pramujianto(175410160)</h3>"
+echo "<h3>Ng. Andre Pramujianto (175410160)</h3>"
 
 ?>
